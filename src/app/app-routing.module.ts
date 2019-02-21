564a1e44f858f9d55aeab632793552b695f3fa6d
Nikita Kirilchuk <nikirilchuk@gmail.com>
2019-02-21 09:09:30 +0200

    add game files to corp repo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './home/home.module#HomeModule' },
+  { path: 'game', loadChildren: './game/game.module#GameModule' },
+  { path: '', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
